1a5ffbc6a0a126f6dcf483a8f00d2cdc726f3bcb
Arequena <alberto.requena@gmail.com>
2024-04-13 09:03:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 8000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World DevOpsooo!')
+  response.send('Hello World DevOpsoo!')
 })
 
 app.listen(app.get('port'), function() {
